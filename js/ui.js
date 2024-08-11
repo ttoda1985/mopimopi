f6379a435dcd276fe93d93acfaad9e7a1b8cb300
nanunu <33363648+ttoda1985@users.noreply.github.com>
2024-08-12 08:35:36 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -68,14 +68,12 @@ function initOverlay(val) {
         $('[name=rps]').text(l.NAV.main.tt.rps[lang])
         $('[name=notice]').fadeOut(0)
         $('.btn_wrap').show()
-        /*
         for (var i in l.Notice) {
             if (lang == "KR")
                 $('#' + i).html(l.Notice[i][lang])
             else
                 $('#' + i).html(l.Notice[i].EN)
         }
-        */
     } else {
         $('[name=notice]').fadeOut(0)
         $('.btn_wrap').show()
